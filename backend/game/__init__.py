6f9e5e1fdd63f46686d811df024d308ad4bf5165
masonchoey <mason@choey.com>
2025-10-17 16:17:02 -0700

    Updates

--- backend/game/__init__.py
+++
@@ -1,3 +0,0 @@
-# Game logic package
-
-
--- run_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run the Hearts Game backend
-# This script sets up the Python path correctly for the backend module
-
-cd "$(dirname "$0")"
-export PYTHONPATH="$PWD/backend:$PYTHONPATH"
-python -m backend.main
